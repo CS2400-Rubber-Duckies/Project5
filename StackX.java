12567095c87e54709cabfb04c5c50a8f7fcfa3fb
‘Gelila’ <79760415+Gelila21@users.noreply.github.com>
2022-05-03 10:40:35 -0700

    .

--- StackX.java
+++ StackX.java
@@ -1,1 +1,28 @@
+class StackX{
+private final int SIZE = 20;
+private int[] st;
+private int top;
+
+public StackX(){
+//constructor
+st = new int[SIZE]; // make array
+top = -1;
+}
+public void push(int j)// put item on stack
+{ 
+    st[++top] =j;
+}
+public int pop()
+{ return st[top--];}
+//take item off stack
+public int peek()
+{ return st[top];
+}
+
+//peek at top of stack
+public boolean isEmpty()  //true if nothing on stack
+{ return (top ==-1); 
+}
+}
+// end class StackX
 
--- Vertex.java
+++ Vertex.java
@@ -1,1 +1,9 @@
 
+class Vertex{
+public char label;
+public boolean wasVisited;
+// label (e.g. 'A')
+public Vertex(char lab){ // constructor
+label = lab;
+wasVisited = false;}
+} // end class Vertex}
