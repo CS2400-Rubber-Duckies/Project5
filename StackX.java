9ca3e2280b2562c3ec4b1277682f8e3f18fe1962
uhnguyen101 <70361768+uhnguyen101@users.noreply.github.com>
2022-05-09 21:55:03 -0700

    fixed formatting

--- StackX.java
+++ StackX.java
@@ -1,27 +1,32 @@
-class StackX{
-private final int SIZE = 700;
-private int[] size_t;
-private int head;
-public StackX(){
-//constructor
-size_t = new int[SIZE]; // make array
-head = -1;
-}
-public void push(int j)// put item on stack
-{ 
-    size_t[++head] =j;
-}
-public int pop()
-{ return size_t[head--];}
-//take item off stack
-public int peek()
-{ return size_t[head];
-}
+class StackX {
+    private final int SIZE = 700;
+    private int[] size_t;
+    private int head;
 
-//peek at top of stack
-public boolean isEmpty()  //true if nothing on stack
-{ return (head==-1); 
-}
+    public StackX() {
+        // constructor
+        size_t = new int[SIZE]; // make array
+        head = -1;
+    }
+
+    public void push(int j)// put item on stack
+    {
+        size_t[++head] = j;
+    }
+
+    public int pop() {
+        return size_t[head--];
+    }
+
+    // take item off stack
+    public int peek() {
+        return size_t[head];
+    }
+
+    // peek at top of stack
+    public boolean isEmpty() // true if nothing on stack
+    {
+        return (head == -1);
+    }
 }
 // end class StackX
-
